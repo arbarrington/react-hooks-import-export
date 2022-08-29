733f638f14c85ac2c931163f1b8e3c06a13e3dc1
Austin Barrington <arbarrington12@gmail.com>
2022-08-29 16:12:54 -0600

    commit tree ac7a2292fbb598c2b818b0ca08e6075f6b247c8a

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks(); 
+  MesaVerde();// => "42 parks!"
+  console.log({parkTrees, parkWildlife});
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
